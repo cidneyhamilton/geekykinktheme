6990bbb37c7acb9937d4a29d1d3c582bb6918169
cidneyhamilton <chamilton@alum.wellesley.edu>
2015-09-04 08:53:21 -0400

    Blog styling.

--- category-blog.php
+++ category-blog.php
@@ -17,30 +17,33 @@ get_header();
             The GKE Blog
           </h1> 
 
-        <section class="description">Welcome to the GKE blog!</section>
+        <section class="description">
+          <p>
+          <b>Geek</b> and <b>kink</b> intersect in innumerable glorious ways.  Most of the people who are a part of this event love to read, and many of us tend to think - a lot - and sometimes we <b>write about it</b>.
+          </p>
+          <p>
+          This <b>blog</b> is a space for thoughts on geekery, kink, permutations of the two, philosophies, ideas, and sometimes eclectic ramblings.
+          </p>
+        </section>
 
         </header>
 
           <?php while ( have_posts() ) : the_post(); ?>       	 	
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-            <header class="entry-header">
-              <h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-              <h2>by <?php the_author_link(); ?></h2>
-            </header><!-- .entry-header -->
+            
+            <?php get_template_part('_partials/post-header'); ?>
 
     <div class="entry-content">
 
         <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'jme-event-base-theme' ) ); ?>
-        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'jme-event-base-theme' ) . '</span>', 'after' => '</div>' ) ); ?>
+
+        <?php if ( comments_open() ) : ?>
+          <span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'jme_event_base_theme' ), __( '<b>%</b> Replies', 'jme_event_base_theme' ) ); ?></span>
+        <?php endif; // End if comments_open() ?>
 
       </div><!-- .entry-content -->
 
       <footer class="entry-meta">
-        <?php if ( comments_open() ) : ?>
-          <span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'jme_event_base_theme' ), __( '<b>%</b> Replies', 'jme_event_base_theme' ) ); ?></span>
-        <?php endif; // End if comments_open() ?>
-        
         <?php edit_post_link( __( 'Edit', 'jme-event-base-theme' ), '<span class="edit-link">', '</span>' ); ?>
       </footer><!-- .entry-meta -->
     </article><!-- #post-<?php the_ID(); ?> -->
